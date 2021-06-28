268a114979b1428552b3ec068766660ad87a38ba
Carlos Aguirre <craguirre952@gmail.com>
2021-06-28 10:59:57 -0400

    install http gem and input http request

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,16 @@
 class MoviesController < ApplicationController
+  
+  require 'http'
+
+  response = HTTP.get('https://github.com/carlosraguirre/movie-app')
+
+  def index
+    movies = Movie.all
+    render json: movies.as_json
+  end
+  
   def show
-    movie = Movie.find_by(id: 2)
-    render json: movie.as_json
+    movie = Movie.find_by(id: params[:id])
+    render json: product
   end
 end
